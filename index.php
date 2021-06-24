c64f51313407ac9e03ddf17e2dd4cd9cb0f6ec7e
wildsonmacedo <86444474+wildsonmacedo@users.noreply.github.com>
2021-06-24 18:47:28 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "172.31.2.117";
+$username = "root";
+$password = "senha.0099";
+$dbname = "basedb"; 
+
+// Create connection
+$conn= mysqli_connect($servername,$username,$password,$dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected Successfully.";
+?>
